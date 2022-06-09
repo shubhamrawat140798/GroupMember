d1d311ca4bc0c76c2716905a0bf9a74240656de4
shubhamrawat140798 <shubhamrawat140798@gmail.com>
2022-06-09 08:42:33 +0530

    initial commit

--- GroupMember/src/main/java/com/group_member/ChatmessengerApplication.java
+++ GroupMember/src/main/java/com/group_member/ChatmessengerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ChatmessengerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatmessengerApplication.class, args);
